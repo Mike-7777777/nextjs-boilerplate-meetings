0614da41e2b4cb9f339d303b46eb8afda0a2f49e
Mike-7777777 <mike467901371@yahoo.com>
2023-06-02 09:03:59 +0200

    dsfsdf

--- app/page.js
+++ app/page.js
@@ -1,6 +1,20 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+export default async function Page() {
+  const data = await getData();
+ 
+  return <main>
+    {/* 获取用户名 */}
+    <html>
+    <form action="/send-data-here" method="post">
+      <label for="first">First name:</label>
+      <input type="text" id="first" name="first" />
+      <label for="last">Last name:</label>
+      <input type="text" id="last" name="last" />
+      <button type="submit">Submit</button>
+    </form>
+    {/* 显示当前用户名 */}
+    </html>
+  </main>;
 }
