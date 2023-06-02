cb8be04157d88281b9b835b64d2d20671046e818
Mike-7777777 <mike467901371@yahoo.com>
2023-06-02 09:37:54 +0200

    form

--- app/page.js
+++ app/page.js
@@ -1,7 +1,22 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
+function handleSubmit(e) {
+  // Prevent the browser from reloading the page
+  e.preventDefault();
+
+  // Read the form data
+  const form = e.target;
+  const formData = new FormData(form);
+
+  // You can pass formData as a fetch body directly:
+  fetch('/some-api', { method: form.method, body: formData });
+
+  // Or you can work with it as a plain object:
+  const formJson = Object.fromEntries(formData.entries());
+  console.log(formJson);
+}
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -9,41 +24,32 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.js</code>
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
       </div>
+      {/* TODO 显示当前时间表（从服务器获取） */}
 
       <div className={styles.center}>
+      
+      {/* 新用户的时间表提交到服务器 */}
       <form action="/send-data-here" method="post">
-        <label for="first">First name:</label>
-        <input type="text" id="first" name="first" />
-        <label for="last">Last name:</label>
-        <input type="text" id="last" name="last" />
+        {/* name */}
+        <label for="name">
+          Your name: <input type="text" id="first" name="name" />
+        </label>
+        {/* time slot,  7 x 7 = 49 */}
+        <label for="slot">
+          Time Slot: 
+          <input type="checkbox" name="slot_1_8" /> 8-12
+          <input type="checkbox" name="slot_1_8" /> 12-14
+          <input type="checkbox" name="slot_1_8" /> 14-16
+          <input type="checkbox" name="slot_1_8" /> 16-18
+          <input type="checkbox" name="slot_1_8" /> 18-20
+          <input type="checkbox" name="slot_1_8" /> 20-22
+          <input type="checkbox" name="slot_1_8" /> 22-24
+        </label>
+
+        <button type="reset">Reset</button>
         <button type="submit">Submit</button>
       </form>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
       </div>
 
       <div className={styles.grid}>
