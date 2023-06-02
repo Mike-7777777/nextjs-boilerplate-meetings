5266d64238fb270dee998c9193671a65b09348c3
Mike-7777777 <mike467901371@yahoo.com>
2023-06-02 07:55:49 +0200

    dafasd

--- app/page.js
+++ app/page.js
@@ -1,10 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
-  return (
-    <div>
-      <h1>我的会议安排</h1>
-    </div>
-  )
+export default function Page() {
+  return <h1>Hello, Next.js!</h1>;
 }
